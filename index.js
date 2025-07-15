15d1eba0830066efaaf6109a946d8f8e6629c997
br3achedd <63490999+br3achedd@users.noreply.github.com>
2025-07-15 20:46:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Teso panel backend online, ne nyomasszuk a nullát!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server fut a ${PORT}-as porton.`);
+});
